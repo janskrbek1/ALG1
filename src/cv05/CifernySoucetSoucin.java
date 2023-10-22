8c34c7badda9ee98cc6fabd8314f284330e4ba14
Jan Skrbek <skrbeja1@fit.cvut.cz>
2023-10-22 18:47:36 +0200

    Upravy adresarove struktury

---
+++ src/cv05/CifernySoucetSoucin.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cv05;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author skrbj
+ */
+public class CifernySoucetSoucin {
+
+    public static final Scanner sc = new Scanner(System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long cislo;
+        int cifernySoucet, cifernySoucin;
+        byte cifra;
+        
+        System.out.println("Zadavej kladna cela cisla, pro ukonceni nacitani zadej nekladne:");
+        while ((cislo = sc.nextLong()) > 0) {
+            cifernySoucet = 0;
+            cifernySoucin = 1;
+            while (cislo > 0) {
+                cifra = (byte) (cislo % 10);
+                cifernySoucet += cifra;
+                if (cifra > 0) cifernySoucin *= cifra;
+                cislo /= 10;
+            }
+            System.out.format("Ciferny soucet = %d%nCiferny soucin nenulovych cifer = %d%n", cifernySoucet, cifernySoucin);
+        }
+    }
+    
+}
