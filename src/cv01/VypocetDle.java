338698b029336fc0fd37f87f969009c4613c1f9e
Jan Skrbek <jan.skrbek1@tul.cz>
2023-11-03 17:44:45 +0100

    Program z prvniho cviceni presunut do vlastni slozky

---
+++ src/cv01/VypocetDle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Skrbek1-J-f6e9
+ */
+public class VypocetDle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter data");
+        
+        System.out.println("Result:");
+    }
+    
+}
