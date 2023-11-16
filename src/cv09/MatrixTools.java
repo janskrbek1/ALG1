629515876023ae88aeead4c03abbdb09484d3c0c
Jan Skrbek <jan.skrbek1@tul.cz>
2023-11-16 11:06:17 +0100

    Zaklad souboru pro praci s 2D poli

---
+++ src/cv09/MatrixTools.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cv09;
+
+/**
+ * matipulace s maticemi realnych a celych cisel.
+ * 
+ * Vsechny metody predpokladaji, ze kazde dvourozmerne pole 
+ * ma vsechny radky stejne dlouhe.
+ * @author skrbj
+ */
+public final class MatrixTools {
+
+    private MatrixTools() {
+        
+    }
+}
